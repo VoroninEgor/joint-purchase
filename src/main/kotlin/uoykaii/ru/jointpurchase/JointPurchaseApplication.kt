56255d30d6942dcbfe42582299d33fd694a06bfa
Voronin Egor <vegor4956@gmail.com>
2024-11-15 16:21:02 +0300

    init

---
+++ src/main/kotlin/uoykaii/ru/jointpurchase/JointPurchaseApplication.kt
@@ -0,0 +1,11 @@
+package uoykaii.ru.jointpurchase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JointPurchaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<JointPurchaseApplication>(*args)
+}
